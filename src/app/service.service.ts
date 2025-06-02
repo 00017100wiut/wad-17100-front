00c0260a6188bd144c1482a9cee8027f1f17e3c7
00017100wiut <00017100wiut@gmail.com>
2025-06-02 23:13:00 +0500

    implemented api requests service

---
+++ src/app/service.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+const baseUrl = 'http://localhost:5293/api/';
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  details: string;
+  sku: number;
+}
+
+export interface AddedProduct {
+  id: number;
+  name: string;
+  price: number;
+  details: string;
+  sku: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  httpClient = inject(HttpClient);
+  constructor() {}
+
+  getAllProducts() {
+    return this.httpClient.get<Product[]>(baseUrl + 'Products');
+  }
+
+  getProductById(id: number) {
+    return this.httpClient.get<Product>(baseUrl + 'Products/' + id);
+  }
+
+  editProduct(item: AddedProduct) {
+    return this.httpClient.put(baseUrl + 'Products/', item);
+  }
+
+  deleteProduct(id: number) {
+    return this.httpClient.delete(baseUrl + 'Products/' + id);
+  }
+
+  createProduct(item: AddedProduct) {
+    return this.httpClient.post<AddedProduct>(baseUrl + 'Products/', item);
+  }
+}
